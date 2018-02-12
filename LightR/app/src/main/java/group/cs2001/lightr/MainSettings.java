186e02d3b87a6f87d4e02ebf4b6556ab9ab0f710
Mannjamin <mann.ashley@outlook.com>
2018-02-12 09:14:23 +0000

    Fixed App Closing on BackPress
    Fixed an issue with the App closing when pressing BackSpace on the MainSettings Activity.

--- LightR/app/src/main/java/group/cs2001/lightr/MainSettings.java
+++ LightR/app/src/main/java/group/cs2001/lightr/MainSettings.java
@@ -1,5 +1,6 @@
 package group.cs2001.lightr;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
@@ -15,6 +16,14 @@ public class MainSettings extends AppCompatActivity {
         setContentView(R.layout.activity_main_settings);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
+    }
+
+    @Override
+    public void onBackPressed() {
+        //Log.d("CDA", "onBackPressed Called");
+        Intent intent = new Intent(MainSettings.this, MainMenu.class);
+        intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_CLEAR_TASK);
+        startActivity(intent);
     }
 
 }
