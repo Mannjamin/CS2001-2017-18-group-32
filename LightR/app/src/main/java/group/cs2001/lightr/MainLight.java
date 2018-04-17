9cdf38d643f79e9496b18081f1a6be25932a6831
Mannjamin <mann.ashley@outlook.com>
2018-04-17 14:16:47 +0100

    Updated Main Menu Sound Icon.
    Updated the Icon of the Sound Menu Button.
    Former Symbol was a replica of the Temperature Icon.

--- LightR/app/src/main/java/group/cs2001/lightr/MainLight.java
+++ LightR/app/src/main/java/group/cs2001/lightr/MainLight.java
@@ -2,6 +2,7 @@ package group.cs2001.lightr;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.provider.ContactsContract;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.view.View;
@@ -14,8 +15,13 @@ import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
 
+import com.jjoe64.graphview.series.DataPoint;
+import com.jjoe64.graphview.series.LineGraphSeries;
+
 public class MainLight extends AppCompatActivity
         implements NavigationView.OnNavigationItemSelectedListener {
+
+    LineGraphSeries<DataPoint> series;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -32,6 +38,7 @@ public class MainLight extends AppCompatActivity
 
         NavigationView navigationView = (NavigationView) findViewById(R.id.nav_view);
         navigationView.setNavigationItemSelectedListener(this);
+
     }
 
     @Override
