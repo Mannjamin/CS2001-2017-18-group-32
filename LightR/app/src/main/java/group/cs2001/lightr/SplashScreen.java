617df2a74b86f07ce7ef0645ec7ecb3e5499825a
Mannjamin <mann.ashley@outlook.com>
2018-04-23 18:09:08 +0100

    Added functionality to the Splash Screen
    Added functionality to the splash screen.
    Made the SplashScreen the main Launcher in the Manifest.
    
    This code was original created by Marvin and made compatible in our main repository by me.  As such, I have added Marvin as a Co-Author of this commit.
    
    Co-Authored-By: marvinabum1998 <marvinabum1998@users.noreply.github.com>

--- LightR/app/src/main/java/group/cs2001/lightr/SplashScreen.java
+++ LightR/app/src/main/java/group/cs2001/lightr/SplashScreen.java
@@ -16,6 +16,14 @@ public class SplashScreen extends AppCompatActivity {
         requestWindowFeature(Window.FEATURE_NO_TITLE);
         getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,
                 WindowManager.LayoutParams.FLAG_FULLSCREEN);
-        
+        setContentView(R.layout.activity_splash_screen);
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent homeIntent = new Intent(SplashScreen.this, MainMenu.class);
+                startActivity(homeIntent);
+                finish();
+            }
+        }, SPLASH_TIME_OUT);
     }
 }
