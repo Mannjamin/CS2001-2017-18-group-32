1f783f409a7886107e931ca51dbb7afd1d4abcf5
Mannjamin <mann.ashley@outlook.com>
2018-04-23 21:57:37 +0100

    Renamed "StartupSettings" to "MainStartupSettings" and added commented code.
    Renamed file to the correct naming conventions of our testing branch.
    I've also added in the main code for the file, It's commented until I properly import Marvin's activity_main_startup_activty XML code.
    
    I've added Marvin as a Co-Author for their code.
    
    Co-Authored-By: marvinabum1998 <marvinabum1998@users.noreply.github.com>

--- LightR/app/src/main/java/group/cs2001/lightr/MainStartupSettings.java
+++ LightR/app/src/main/java/group/cs2001/lightr/MainStartupSettings.java
@@ -16,6 +16,40 @@ import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.Spinner;
 
-public class StartupSettings extends AppCompatActivity {
+public class MainStartupSettings extends AppCompatActivity {
 
+    private Button Continue;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main_startup_settings);
+
+//        Continue = (Button) findViewById(R.id.btnContinue);
+//
+//        Spinner spinner = (Spinner) findViewById(R.id.spinner);
+//        Spinner spinner2 = (Spinner) findViewById(R.id.spinner2);
+//
+//        ArrayAdapter<String> myAdapter = new ArrayAdapter<String>(StartupSettings.this,
+//                android.R.layout.simple_list_item_1, getResources().getStringArray(R.array.languages));
+//        myAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+//        spinner.setAdapter(myAdapter);
+//
+//        ArrayAdapter<String> myAdapter2 = new ArrayAdapter<String>(StartupSettings.this,
+//                android.R.layout.simple_list_item_1, getResources().getStringArray(R.array.accessibility));
+//        myAdapter2.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+//        spinner2.setAdapter(myAdapter2);
+//
+//        Continue.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View view) {
+//                openConnectionPage();
+//            }
+//        });
+    }
+
+    public void openConnectionPage() {
+//        Intent intent5 = new Intent(this, ConnectionPage.class);
+//        startActivity(intent5);
+    }
 }
