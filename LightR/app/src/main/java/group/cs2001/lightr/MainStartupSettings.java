d92538d7aca8e4d2759947769b54f7a9e6e20164
Mannjamin <mann.ashley@outlook.com>
2018-04-23 21:52:13 +0100

    Created activity_main_startup_settings layout XML for Marvin's Code
    Created activity_main_startup_settings layout XML for Marvin's Code
    
    I have also begun preperations to add Marvin's MainStartupSettings java file code.
    
    Added Marvin as Co-Author for commits.
    
    Co-Authored-By: marvinabum1998 <marvinabum1998@users.noreply.github.com>

--- LightR/app/src/main/java/group/cs2001/lightr/MainStartupSettings.java
+++ LightR/app/src/main/java/group/cs2001/lightr/MainStartupSettings.java
@@ -1,4 +1,21 @@
 package group.cs2001.lightr;
 
-public class MainStartupSettings {
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.Spinner;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.Spinner;
+
+public class StartupSettings extends AppCompatActivity {
+
 }
