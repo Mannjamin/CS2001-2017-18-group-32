afe928c9af9c70855b0d1841b45b4f2569e439c3
Samuel Caire Ribeiro <1624508@brunel.ac.uk>
2018-04-24 14:03:03 +0100

    Revert "Merge branch 'testing' of https://github.com/BrunelCS/CS2001-2017-18-group-32 into testing"
    This reverts commit 5ac32241f2faf281aae37204da893d65408a4d05, reversing
    changes made to 710cbed1344351ee71fc9e47f790470c4175d188.

--- LightR/app/src/main/java/group/cs2001/lightr/MainMenu.java
+++ LightR/app/src/main/java/group/cs2001/lightr/MainMenu.java
@@ -3,7 +3,6 @@ package group.cs2001.lightr;
 import android.app.AlertDialog;
 import android.content.DialogInterface;
 import android.content.Intent;
-import android.graphics.Color;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
